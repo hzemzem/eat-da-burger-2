ce4cc45b7713bf0a655cc76032d77bc959ae43f7
anas tellat <anas tellat>
2017-08-23 12:22:50 -0400

    initial commit

---
+++ models/customer.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+  var Customer = sequelize.define("Customer", 
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    }, {
+      classMethods: {
+        associate: function(models) {
+          Customer.hasMany(models.Burger)
+        }
+      }
+    });
+
+  return Customer;
+};
